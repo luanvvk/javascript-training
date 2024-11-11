28f220e0360ae6ca0a4fc9484a87738e04952b31
Luan Vu <116854422+luanvvk@users.noreply.github.com>
2024-11-11 23:03:22 +0700

    js basic practice

---
+++ exercise/src/comparison.js
@@ -0,0 +1,25 @@
+let a =1;
+let b='1';
+if(a==b) {
+    console.log('true');
+} else {
+    console.log('false');
+};
+
+if(a!=b) {
+    console.log('true');
+} else {
+    console.log('false');
+};
+
+if(a===b) {
+    console.log('true');
+} else {
+    console.log('false');
+};
+
+if(a!==b) {
+    console.log('true');
+} else {
+    console.log('false');
+};
---
+++ exercise/src/types.js
@@ -0,0 +1,41 @@
+let a;
+a = 5;
+var b = 'luan vu';
+var isSuccess = true;
+let age;
+const isNull = null;
+var id = Symbol(`id`);
+var myFunction = function() {
+}
+var myCat = {
+    name: 'leon',
+    age: 1,
+    type: 'siberia',
+    myFunction: function(){       
+    }
+};
+let myArray = [
+    'Java',
+    'C#',
+    'C++',
+    'Javascript'
+]
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+
+console.log('name', b);
+console.log(a + b);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof isSuccess);
+console.log(typeof isNull);
+console.log(typeof age);
+console.log(typeof id);
+console.log('myFunction', myFunction);
+console.log(typeof myFunction);
+console.log(myCat);
+console.log(typeof myCat);
+console.log(myArray);
+console.log(typeof myArray);
