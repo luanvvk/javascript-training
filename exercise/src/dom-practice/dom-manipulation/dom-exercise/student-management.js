8d5a4abe2364fb28f9936cd9e45354f29f9e5a9f
Luan Vu <116854422+luanvvk@users.noreply.github.com>
2024-12-01 23:40:44 +0700

    add localStorage, add edit feature

--- exercise/src/dom-practice/dom-manipulation/dom-exercise/student-management.js
+++ exercise/src/dom-practice/dom-manipulation/dom-exercise/student-management.js
@@ -1,109 +1,3 @@
-let fullname = document.getElementById('fullname');
-let email = document.getElementById('email');
-let phone = document.getElementById('phone-number');
-let address = document.getElementById('address');
-const male = document.querySelector('#male');
-const female = document.querySelector('#female');
-let editingIndex = null;
-
-const checkFullName = () => {
-  let valid = false;
-  const fullnameTestString = fullname.value;
-  if (!isEmpty(fullnameTestString)) {
-    showError(fullname, 'Name cannot be blank');
-  } else if (fullnameTestString.trim().length < 3) {
-    showError(fullname, 'Full name needs to be longer than 3 characters');
-  } else {
-    showSuccess(fullname);
-    valid = true;
-  }
-  return valid;
-};
-
-const checkEmail = () => {
-  let valid = false;
-  const emailTestString = email.value.trim();
-  if (!isEmpty(emailTestString)) {
-    showError(email, 'Email cannot be blank');
-  } else if (!isEmailValid(emailTestString)) {
-    showError(email, 'Email is invalid');
-  } else {
-    showSuccess(email);
-    valid = true;
-  }
-};
-
-const checkAddress = () => {
-  let valid = false;
-  const addressTestString = address.value.trim();
-  if (!isEmpty(addressTestString)) {
-    showError(address, 'Please enter your address');
-  } else {
-    showSuccess(address);
-    valid = true;
-  }
-};
-
-let gender = '';
-function setValue1() {
-  male.value = 1;
-  gender = male.value;
-}
-function setValue2() {
-  female.value = 2;
-  gender = female.value;
-}
-const checkGender = () => {
-  let valid = false;
-
-  if (gender === '') {
-    document.querySelector('#gender-error').innerHTML = 'Please identify your gender';
-  } else {
-    document.querySelector('#gender-error').innerHTML = '';
-    valid = true;
-  }
-};
-
-const checkPhoneNumber = () => {
-  let valid = false;
-  const phoneNumTestString = phone.value;
-  if (!isEmpty(phoneNumTestString)) {
-    showError(phone, 'Please enter your phone number');
-  } else if (phoneNumTestString.trim().length > 10) {
-    showError(phone, 'Phone number should have less than 10 digits');
-  } else if (!isPhoneNumberCorrect(phoneNumTestString)) {
-    showError(phone, 'Please enter number only');
-  } else {
-    showSuccess(phone);
-    valid = true;
-  }
-};
-
-const isEmpty = (value) => (value === '' ? false : true);
-const isEmailValid = (emailTestString) => {
-  const testString = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
-  return testString.test(emailTestString);
-};
-const isPhoneNumberCorrect = (phoneNumTestString) => {
-  const testString = /^[0-9]+$/;
-  return testString.test(phoneNumTestString);
-};
-const showError = (input, message) => {
-  const formField = input.parentElement;
-  formField.classList.remove('success');
-  formField.classList.add('error');
-  const error = formField.querySelector('span');
-  error.textContent = message;
-};
-
-const showSuccess = (input) => {
-  const formField = input.parentElement;
-  formField.classList.remove('error');
-  formField.classList.add('success');
-  const success = formField.querySelector('span');
-  success.textContent = '';
-};
-
 let form = document.querySelector('.student-form');
 form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -113,58 +7,35 @@ form.addEventListener('submit', function (event) {
   let isAddressCorrect = checkAddress();
   let isGenderValid = checkGender();
 
-  let students = localStorage.getItem('students')
-    ? JSON.parse(localStorage.getItem('students'))
-    : [];
-  function addStudent(fullname, email, phone, address, gender) {
-    const student = {
-      fullname: fullname.value,
-      email: email.value,
-      phone: phone.value,
-      address: address.value,
-      gender: gender,
-    };
-    students.push(student);
-  }
-
-  if (editingIndex !== null) {
-    students[editingIndex] = { fullname, email, phone, address, gender };
-    editingIndex = null;
-  } else {
-    addStudent(fullname, email, phone, address, gender);
-  }
-
-  localStorage.setItem('students', JSON.stringify(students));
-  renderStudentList();
-
   let isFormValid =
     isFullNameValid && isEmailValid && isPhoneNumCorrect && isAddressCorrect && isGenderValid;
   if (isFormValid) {
   }
+
+  let students = localStorage.getItem('students')
+    ? JSON.parse(localStorage.getItem('students'))
+    : [];
+
+  const student = {
+    fullname: fullname.value,
+    email: email.value,
+    phone: phone.value,
+    address: address.value,
+
+    gender: gender,
+  };
+  students.push(student);
+  if (currentIndex !== -1) {
+    students[currentIndex] = student;
+    currentIndex = -1;
+    document.querySelector('button[type="submit"]').innerHTML = 'Submit';
+  } else {
+    renderStudentList(student);
+  }
+
+  localStorage.setItem('students', JSON.stringify(students));
 });
 
-form.addEventListener('input', function (e) {
-  switch (e.target.id) {
-    case 'fullname':
-      checkFullName();
-      break;
-    case 'email':
-      checkEmail();
-      break;
-    case 'address':
-      checkAddress();
-      break;
-    case 'phone-number':
-      checkPhoneNumber();
-      break;
-    case 'male':
-      checkGender();
-      break;
-    case 'female':
-      checkGender();
-      break;
-  }
-});
 function renderStudentList() {
   let students = localStorage.getItem('students')
     ? JSON.parse(localStorage.getItem('students'))
@@ -183,11 +54,13 @@ function renderStudentList() {
           <th>Address</th>
           <th>Action</th>
         </tr>`;
+
   students.forEach((student, index) => {
     let studentId = index;
     index++;
     let genderLabel = parseInt(student.gender) === 1 ? 'Male' : 'Female';
-    tableContent += `<tr>
+
+    tableContent += `<tr id="data-list">
                 <td>${index}</td>
                 <td>${student.fullname}</td>
                 <td>${student.email}</td>
@@ -200,19 +73,22 @@ function renderStudentList() {
                 </td>
               </tr>`;
   });
-  document.getElementById('students-grids').innerHTML = tableContent;
+  document.getElementById('student-grids').innerHTML = tableContent;
   form.reset();
 }
 function deleteStudent(id) {
   let students = localStorage.getItem('students')
     ? JSON.parse(localStorage.getItem('students'))
     : [];
+
   students.splice(id, 1);
+
   localStorage.setItem('students', JSON.stringify(students));
   renderStudentList();
 }
-
+let currentIndex = -1;
 function editStudent(index) {
+  currentIndex = index;
   let students = localStorage.getItem('students')
     ? JSON.parse(localStorage.getItem('students'))
     : [];
@@ -221,8 +97,5 @@ function editStudent(index) {
   document.getElementById('email').value = student.email;
   document.getElementById('phone-number').value = student.phone;
   document.getElementById('address').value = student.address;
-  document.querySelector('button[type=submit]').innerText = 'Save';
-  editingIndex = index;
-
-  localStorage.setItem('students', JSON.stringify(students));
+  document.querySelector('button[type="submit"]').innerHTML = 'Save';
 }
---
+++ exercise/src/dom-practice/dom-manipulation/dom-exercise/student-validation.js
@@ -0,0 +1,126 @@
+let fullname = document.getElementById('fullname');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone-number');
+let address = document.getElementById('address');
+const male = document.querySelector('#male');
+const female = document.querySelector('#female');
+
+const checkFullName = () => {
+  let valid = false;
+  const fullnameTestString = fullname.value;
+  if (!isEmpty(fullnameTestString)) {
+    showError(fullname, 'Name cannot be blank');
+  } else if (fullnameTestString.trim().length < 3) {
+    showError(fullname, 'Full name needs to be longer than 3 characters');
+  } else {
+    showSuccess(fullname);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkEmail = () => {
+  let valid = false;
+  const emailTestString = email.value.trim();
+  if (!isEmpty(emailTestString)) {
+    showError(email, 'Email cannot be blank');
+  } else if (!isEmailValid(emailTestString)) {
+    showError(email, 'Email is invalid');
+  } else {
+    showSuccess(email);
+    valid = true;
+  }
+};
+
+const checkAddress = () => {
+  let valid = false;
+  const addressTestString = address.value.trim();
+  if (!isEmpty(addressTestString)) {
+    showError(address, 'Please enter your address');
+  } else {
+    showSuccess(address);
+    valid = true;
+  }
+};
+
+let gender = '';
+function setValue1() {
+  male.value = 1;
+  gender = male.value;
+}
+function setValue2() {
+  female.value = 2;
+  gender = female.value;
+}
+const checkGender = () => {
+  let valid = false;
+
+  if (gender === '') {
+    document.querySelector('#gender-error').innerHTML = 'Please identify your gender';
+  } else {
+    document.querySelector('#gender-error').innerHTML = '';
+    valid = true;
+  }
+};
+
+const checkPhoneNumber = () => {
+  let valid = false;
+  const phoneNumTestString = phone.value;
+  if (!isEmpty(phoneNumTestString)) {
+    showError(phone, 'Please enter your phone number');
+  } else if (phoneNumTestString.trim().length > 10) {
+    showError(phone, 'Phone number should have less than 10 digits');
+  } else if (!isPhoneNumberCorrect(phoneNumTestString)) {
+    showError(phone, 'Please enter number only');
+  } else {
+    showSuccess(phone);
+    valid = true;
+  }
+};
+
+const isEmpty = (value) => (value === '' ? false : true);
+const isEmailValid = (emailTestString) => {
+  const testString = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+  return testString.test(emailTestString);
+};
+const isPhoneNumberCorrect = (phoneNumTestString) => {
+  const testString = /^[0-9]+$/;
+  return testString.test(phoneNumTestString);
+};
+const showError = (input, message) => {
+  const formField = input.parentElement;
+  formField.classList.remove('success');
+  formField.classList.add('error');
+  const error = formField.querySelector('span');
+  error.textContent = message;
+};
+
+const showSuccess = (input) => {
+  const formField = input.parentElement;
+  formField.classList.remove('error');
+  formField.classList.add('success');
+  const success = formField.querySelector('span');
+  success.textContent = '';
+};
+form.addEventListener('input', function (e) {
+  switch (e.target.id) {
+    case 'fullname':
+      checkFullName();
+      break;
+    case 'email':
+      checkEmail();
+      break;
+    case 'address':
+      checkAddress();
+      break;
+    case 'phone-number':
+      checkPhoneNumber();
+      break;
+    case 'male':
+      checkGender();
+      break;
+    case 'female':
+      checkGender();
+      break;
+  }
+});
