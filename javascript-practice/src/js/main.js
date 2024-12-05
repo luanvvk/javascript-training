8e6b22cf2f04f69437b805bdb91aa3d3b96d770c
Luan Vu <luan.vvk@gmail.com>
2024-12-05 14:48:12 +0700

    update effect for the side nav bar

---
+++ javascript-practice/src/js/main.js
@@ -0,0 +1,19 @@
+let list = document.querySelectorAll('.navbar--list__item');
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove('hovered');
+  });
+  this.classList.add('hovered');
+}
+list.forEach((item) => item.addEventListener('mouseover', activeLink));
+// Menu Toggle
+let toggle = document.querySelector('.menu-bar-toggle');
+let sideNavbar = document.querySelector('.side-navbar');
+let mainBody = document.querySelector('.main-body');
+let appLogo = document.querySelector('.app-logo');
+
+toggle.onclick = function () {
+  sideNavbar.classList.toggle('active');
+  mainBody.classList.toggle('active');
+  appLogo.classList.toggle('active');
+};
